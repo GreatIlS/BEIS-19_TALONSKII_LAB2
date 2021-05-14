6f436283db9dac139715d4f3af94b63b5ee0ed66
ilsver2 <thisisgoood@ya.ru>
2021-05-14 09:42:42 +0300

    lab 1

---
+++ lb 2 Talonsky/lb 2 Talonsky/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lb_1_Talonsky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = ("Задание 2");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            try
+            {
+                Console.WriteLine("Введите длину ребра куба = ");
+                double x = Double.Parse(Console.ReadLine());
+                if (x > 0)
+                {
+                    double y;
+                    double z;
+                    y = x*x*x;
+                    z = x*x*6;
+                    Console.WriteLine("Обьём куба = " + y);
+                    Console.WriteLine("Площадь его поверхности = " + z);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Введены некорректные данные");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Введены некорректные данные.");
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
